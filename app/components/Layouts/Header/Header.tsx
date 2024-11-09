f2e416ebda3acedbaeb85c59fbfe247a7cbf7a9a
kake <Makky.rrr.omach@yahoo.ne.jp>
2024-11-10 02:45:46 +0900

    profile

--- app/components/Layouts/Header/Header.tsx
+++ app/components/Layouts/Header/Header.tsx
@@ -1,5 +1,4 @@
 'use client';
-import React from 'react';
 import Link from 'next/link';
 import { useHeadStore } from '@/features/store';
 
@@ -9,10 +8,10 @@ const Header = () => {
   return (
      <nav className="w-[30%] absolute top-[3%] right-[3%] z-10">
         <ul className="border-b border-[#df0000] flex justify-around text-3xl">
-          {!(currentPage === 'Top') && <li><Link href="/">Top</Link></li>}
-          {!(currentPage === 'Profile') && <li><Link href="/profile">Profile</Link></li>}
-          {!(currentPage === 'Works') && <li><Link href="/works">Works</Link></li>}
-          {!(currentPage === 'Contact') && <li><Link href="/contact">Contact</Link></li>}
+          {!(currentPage === 'Top') && <li className="hover:bg-white hover:opacity-70"><Link href="/">Top</Link></li>}
+          {!(currentPage === 'Profile') && <li className="hover:bg-white hover:opacity-70"><Link href="/profile">Profile</Link></li>}
+          {!(currentPage === 'Works') && <li className="hover:bg-white hover:opacity-70"><Link href="/works">Works</Link></li>}
+          {!(currentPage === 'Contact') && <li className="hover:bg-white hover:opacity-70"><Link href="/contact">Contact</Link></li>}
         </ul>
       </nav>
   );
--- features/components/page/Profile/Foot.tsx
+++ features/components/page/Profile/Foot.tsx
@@ -1,11 +1,35 @@
-import Image from 'next/image'
+import Link from 'next/link';
 
 const Foot = () => {
+  const sideLinks = [
+    {name: 'Top', url: '/'}, 
+    {name: 'Works', url: '/works'},
+    {name: 'Contact', url: '/contact'}
+  ];
+  const topLinks = [
+    {name: 'Instagram', url: 'https://www.instagram.com/kake__photo/'}, 
+    {name: 'X', url: 'https://x.com/kake_webDesEng'},
+    {name: 'Pixta', url: 'https://creator.pixta.jp/@prof2547496?_gl=1*wdrj94*_gcl_au*ODMyNTMzODc4LjE3MzExNzMyMzA.*_ga*MTIwNDU2NzI2MS4xNzMxMTczMjMx*_ga_VBSX8RFDG5*MTczMTE3MzIzMS4xLjEuMTczMTE3MzI0Mi40OS4wLjA.&_ga=2.112376640.593139733.1731173231-1204567261.1731173231'},
+    {name: 'GitHub', url: 'https://github.com/kake-p'},
+    {name: 'CrowdWorks', url: 'https://crowdworks.jp/public/employees/5693653'}
+  ];
+
   return (
-    <footer className="flex flex-col items-center gap-[30%] h-96">
-      <div className="flex flex-col">
-        <p className="self-center"><a href="">↑<br/>Back to Top</a></p>
-        <p className="self-end">Last Updated: 2024/09/01</p>
+    <footer className="border relative bg-black text-white">
+      <nav className="absolute top-[50%] -translate-y-1/2 ml-8">
+        <ul className="flex flex-col gap-2 border-l border-[#df0000] pl-4">
+          {sideLinks.map((link, index) => <li className="hover:bg-black hover:opacity-70" key={index}><Link href={link.url}>{ link.name }</Link></li>)}
+        </ul>
+      </nav>
+
+      <div>
+        <nav className="flex flex-col">
+          <ul className="flex gap-16 border-b border-[#df0000] px-8 ml-auto mt-2">
+            {topLinks.map((link, index) => <li className="hover:bg-black hover:opacity-70" key={index}><Link href={link.url} rel="noopener noreferrer" target="_blank">{ link.name }</Link></li>)}
+          </ul>
+        </nav>
+          
+        <p className="self-center text-center mt-10 hover:bg-black hover:opacity-70"><a href="">↑<br/>Back to Top</a></p>
       </div>
     </footer>
   )
